48a911923756d4b7ce4b47051975173b78b2549d
nathaliedpettersson <89846223+nathaliedpettersson@users.noreply.github.com>
2022-03-16 11:33:20 +0100

    Added stuff and tried things

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,19 @@ document.body.append(img);
 // img.src = './img/drake.jpeg'; Detta funkar inte!
 
 const imgUrl = new URL('./img/drake.jpeg', import.meta.url );
-img.src = imgUrl.toString();
+img.src = imgUrl.toString(); // href works too
 console.log(imgUrl);
+
+// Clara kollar upp detta, funkade ej att lägga upp fler bilder
+
+// const imgNames = ['pad_thai', 'newyork', 'drake'];
+
+// for(let i:number = 0; i<imgNames.length; i++) {
+//     const img = document.createElement('img');
+//     document.body.append(img);
+//     const string = `./img/${imgNames[i]}`;
+//     const imgUrl = new URL(string, import.meta.url );
+//     console.log(imgUrl);
+//     img.src = imgUrl.href;
+// }
+
